1733e69ca772c373d14214d1a196a25ea938134b
mary-el <mary905el@gmail.com>
2024-06-13 21:40:27 +0500

    Recognition modes added

---
+++ modes/face_recognition.py
@@ -0,0 +1,53 @@
+import glob
+import os
+import pickle
+
+import face_recognition as fr
+import numpy as np
+
+from config import config
+from utils import face_in_area
+import tqdm
+
+
+def encode_folder():
+    save_to = config['face-recognition']['embedding_folder']
+    print(save_to)
+    os.makedirs(save_to, exist_ok=True)
+    for file in tqdm.tqdm(glob.glob(config['images_folder'] + '/*')):
+        enc_file = f'{save_to}/{os.path.basename(file).split(".")[0]}'
+        if not os.path.exists(enc_file):
+            image = fr.load_image_file(file)
+            encoding = fr.face_encodings(image, num_jitters=config['face-recognition']['num_jitters'])[0]
+            with open(enc_file, 'wb') as f:
+                pickle.dump(encoding, f)
+
+
+def search_for_faces(id_to_encoding, small_frame, left_area, right_area):
+    face_locations = fr.face_locations(small_frame)
+    if len(face_locations) == 0:
+        return [], [], -1, None
+    # face_locations
+    face_encodings = fr.face_encodings(small_frame, face_locations)
+    face_locations = np.array(face_locations)
+    face_locations[:, [1, 3]] = face_locations[:, [3, 1]]
+    recognized_names = []
+    open_door = -1
+    open_name = None
+    names = list(id_to_encoding.keys())
+    encodings = list(id_to_encoding.values())
+    for i, face_encoding in enumerate(face_encodings):
+        matches = fr.compare_faces(encodings, face_encoding, tolerance=config['face-recognition']['tolerance'])
+        name = 0
+        face_distances = fr.face_distance(encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = names[best_match_index]
+            if face_in_area(face_locations[i], left_area):
+                open_name = name
+                open_door = 2
+            elif face_in_area(face_locations[i], right_area):
+                open_name = name
+                open_door = 1
+        recognized_names.append(name)
+    return recognized_names, face_locations, open_door, open_name
---
+++ utils.py
@@ -0,0 +1,114 @@
+import json
+import pickle
+from datetime import datetime
+
+import pandas as pd
+import requests
+
+from config import config
+
+connection_config = config['connection']
+
+headers = {
+    "Content-type": "application/json; charset=UTF-8",
+    "Authorization": "Bearer null"
+}
+
+previous_state = {
+    'id': -1,
+    'direction': 0
+}
+
+
+def set_headers():
+    global headers
+    headers["Authorization"] = getToken(connection_config['host'])
+
+
+def getToken(host):  # авторизация
+    url = f'http://{host}/api/system/auth'
+    payload = {
+        "login": connection_config['login'],
+        "password": connection_config['password']
+    }
+    return requests.request("post", url, json=payload, headers=headers).json()["token"]
+
+
+def read_users():  # Получение списка сотрудников
+    url = f'http://{connection_config["host"]}/api/users/staff/list'
+    querystring = {"withPhone": "true"}
+    response = pd.read_json(requests.request("get", url, headers=headers, params=querystring))[['id', 'name']]
+    return response.text
+
+
+def passing(user_id, direction, event_description='Камера'):
+    '''
+    direction 1 на вход 2 на выход  направление  открытие турникета
+    '''
+    url = f'http://{connection_config["host"]}/api/devices/{config["tourniquet"]["id_tur"]}/pass'
+    payload = {
+        "user_id": user_id,
+        "direction": direction,
+        "event_description": event_description
+    }
+    response = requests.request("post", url, json=payload, headers=headers)
+    # print(response.text)
+    # return (pd.read_json(response.text).get('result'))
+    return (json.loads(response.text).get('result'))
+
+
+def open_doors(id, direction, dict_users):
+    doors = ['вход', 'выход']
+    print(f'В {datetime.now().strftime("%D:%H:%M:%S")}  {doors[direction - 1]}  {dict_users[id]}! ')
+    if not (id == previous_state['id'] and direction == previous_state['direction']):
+        previous_state['id'] = id
+        previous_state['direction'] = direction
+        if (passing(int(id), direction, f'камера {direction}') == 'ok'):  # моя откравает проход
+            pass
+        else:
+            exit()  # скорее всего просрочен токен авторизации выходим и заходим снова
+
+    # print(read_db())
+
+
+def read_db1():
+    db = {}
+    url = f'http://{connection_config["host"]}/api/users/staff/list'
+    querystring = {"withPhone": "true"}
+    for id, name in pd.read_json(requests.request("get", url, headers=headers, params=querystring).text)[
+        ['id', 'name']].values:
+        db[id] = name
+    print(db)
+    return db
+
+
+def read_db():
+    db = {}
+    url = f'http://{connection_config["host"]}/api/users/staff/list'
+    querystring = {"withPhone": "true"}
+    for user in json.loads(requests.request("get", url, headers=headers, params=querystring).text):
+        db[user['id']] = user['name']
+    return db
+
+
+def read_excel(file):
+    df = pd.read_excel(file)
+    user_dict = {ind: name for ind, name in df.values}
+    return user_dict
+
+
+def get_encodings(dict_users, encoded_path):
+    id_to_encoding = {}
+    for id in dict_users.keys():
+        if id == 0:
+            continue
+        path = f'{encoded_path}/{id}'
+        with open(path, 'rb') as file:
+            encoding = pickle.load(file)
+        id_to_encoding[id] = encoding
+    return id_to_encoding
+
+
+def face_in_area(face_location, area):
+    return area[0] < (face_location[3] + face_location[1]) // 2 < area[2] and area[1] < (
+            face_location[0] + face_location[2]) // 2 < area[3]
