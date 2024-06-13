1734a211836a8be732704852af65e53f11ebc5d9
mary-el <mary905el@gmail.com>
2024-06-13 21:40:27 +0500

    Recognition modes added

--- main.py
+++ main.py
@@ -1,127 +1,65 @@
 import argparse
-import glob
-import json
-import os
-import pickle
 from datetime import datetime
 
 import cv2
-import face_recognition as fr
-import numpy as np
-import pandas as pd
-import requests
 
-from config import tolerance, num_jitters, host, id_tur, headers, previous_state, NoName, X0, X1, W0, W1, Y0, Y1, H0, H1
+from config import config
 
+area_1 = config['tourniquets']['area_1']
+area_2 = config['tourniquets']['area_2']
 
-def getToken():  # авторизация
-    url = f'http://{host}/api/system/auth'
-    payload = {
-        "login": "api",
-        "password": "1q2w3e4R"
-    }
-    return requests.request("post", url, json=payload, headers=headers).json()["token"]
+if config['mode'] == 'face-recognition':
+    from modes.face_recognition import encode_folder, search_for_faces
+elif config['mode'] == 'facenet':
+    from modes.facenet import encode_folder, search_for_faces
+else:
+    print(f'Unsupported mode: {config["mode"]}')
+    exit()
 
-
-def passing(user_id, direction, event_description='Камера'):
-    '''
-    direction 1 на вход 2 на выход  направление  открытие турникета
-    '''
-    url = f'http://{host}/api/devices/{id_tur}/pass'
-    payload = {
-        "user_id": user_id,
-        "direction": direction,
-        "event_description": event_description
-    }
-    response = requests.request("post", url, json=payload, headers=headers)
-    # print(response.text)
-    # return (pd.read_json(response.text).get('result'))
-    return (json.loads(response.text).get('result'))
+from utils import set_headers, open_doors, read_db, read_excel, get_encodings
 
 
-def read_users():  # Получение списка сотрудников
-    url = f'http://{host}/api/users/staff/list'
-    querystring = {"withPhone": "true"}
-    response = pd.read_json(requests.request("get", url, headers=headers, params=querystring))[['id', 'name']]
-    return response.text
-
-
-def show_recognized_faces(frame, face_locations, recognized_names, reduce_frame, left_area, right_area):
-    cv2.rectangle(frame, (left_area[0], left_area[1]), (left_area[2], left_area[3]), (0, 255, 255), 2)
-    cv2.rectangle(frame, (right_area[0], right_area[1]), (right_area[2], right_area[3]), (255, 0, 0), 2)
-    for (top, right, bottom, left), name in zip(face_locations, recognized_names):
+def show_recognized_faces(frame, face_locations, recognized_ids, reduce_frame, left_area, right_area):
+    cv2.rectangle(frame, (left_area[0] * reduce_frame, left_area[1] * reduce_frame), (left_area[2] * reduce_frame, left_area[3] * reduce_frame), (0, 255, 255), 2)
+    cv2.rectangle(frame, (right_area[0] * reduce_frame, right_area[1] * reduce_frame), (right_area[2] * reduce_frame, right_area[3] * reduce_frame), (255, 0, 0), 2)
+    for (top, left, bottom, right), id in zip(face_locations, recognized_ids):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= reduce_frame
-        right *= reduce_frame
-        bottom *= reduce_frame
-        left *= reduce_frame
-
+        top, right, bottom, left = [int(x * reduce_frame) for x in (top, right, bottom, left)]
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
         # Draw a label with a name below the face
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_COMPLEX
-        cv2.putText(frame, dict_users[name] if name != NoName else NoName, (left + 6, bottom - 6), font, 0.5,
+        cv2.putText(frame,
+                    dict_users[id],
+                    (left + 6, bottom - 6), font, 0.5,
                     (255, 255, 255), 1)
     return frame
 
 
-def open_doors(id, direction):
-    doors = ['вход', 'выход']
-    print(f'В {datetime.now().strftime("%D:%H:%M:%S")}  {doors[direction - 1]}  {dict_users[id]}! ')
-    if not (id == previous_state['id'] and direction == previous_state['direction']):
-        previous_state['id'] = id
-        previous_state['direction'] = direction
-        if (passing(int(id), direction, f'камера {direction}') == 'ok'):  # моя откравает проход
-            pass
-        else:
-            exit()  # скорее всего просрочен токен авторизации выходим и заходим снова
-
-    # print(read_db())
-
-
-def video_capture(encodings, names, camera, reduce_frame=2, show=True):
+def video_capture(id_to_encoding, camera, dict_users, reduce_frame=2, show=True, test=False):
     cap = cv2.VideoCapture(camera)
     cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
     _, frame = cap.read()
-    width, height = frame.shape[1], frame.shape[0]
-    left_area = [int(X0 * width), int(Y0 * height), int((X0 + W0) * width), int((Y0 + H0) * height)]
-    right_area = [int(X1 * width), int(Y1 * height), int((X1 + W1) * width), int((Y1 + H1) * height)]
+    width, height = frame.shape[1] / reduce_frame, frame.shape[0] / reduce_frame
+    left_area = [int(area_1[0] * width), int(area_1[1] * height), int((area_1[0] + area_1[2]) * width), int((area_1[1] + area_1[3]) * height)]
+    right_area = [int(area_2[0] * width), int(area_2[1] * height), int((area_2[0] + area_2[2]) * width), int((area_2[1] + area_2[3]) * height)]
     stop = False
     while not stop:
         ret, frame = cap.read()
         small_frame = cv2.resize(frame, (0, 0), fx=1 / reduce_frame, fy=1 / reduce_frame)
-        face_locations = fr.face_locations(small_frame)
-        face_encodings = fr.face_encodings(small_frame, face_locations)
 
-        recognized_names = []
-        open_door = -1
-        open_name = None
-        for i, face_encoding in enumerate(face_encodings):
-            matches = fr.compare_faces(encodings, face_encoding, tolerance=tolerance)
-            name = NoName
-            face_distances = fr.face_distance(encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = names[best_match_index]
-                if (left_area[0] < (face_locations[i][3] + face_locations[i][1]) // 2 * reduce_frame < left_area[2] and
-                        left_area[1] < (face_locations[i][0] + face_locations[i][2]) // 2 * reduce_frame < left_area[
-                            3]):
-                    open_name = name
-                    open_door = 2
-                elif (right_area[0] < (face_locations[i][3] + face_locations[i][1]) // 2 * reduce_frame < right_area[
-                    2] and
-                      right_area[1] < (face_locations[i][0] + face_locations[i][2]) // 2 * reduce_frame < right_area[
-                          3]):
-                    open_name = name
-                    open_door = 1
-            recognized_names.append(name)
+        recognized_ids, face_locations, open_door, user_ind = search_for_faces(id_to_encoding, small_frame, left_area,
+                                                                                 right_area)
         if open_door != -1:
-            open_doors(open_name, open_door)
+            if test:
+                print(f'Door #{open_door} opened for {", ".join(dict_users[i] for i in recognized_ids if i != 0)}')
+            else:
+                open_doors(user_ind, open_door, dict_users)
         if show:
             cv2.imshow('Face recognition',
-                       show_recognized_faces(frame, face_locations, recognized_names, reduce_frame, left_area,
+                       show_recognized_faces(frame, face_locations, recognized_ids, reduce_frame, left_area,
                                              right_area))
         if cv2.waitKey(1) == 13:  # 13 is the Enter Key
             stop = True
@@ -130,72 +68,24 @@ def video_capture(encodings, names, camera, reduce_frame=2, show=True):
     cv2.destroyAllWindows()
 
 
-def read_db0(file):
-    df = pd.read_excel(file)
-    return df
-
-
-def read_db1():
-    db = {}
-    url = f'http://{host}/api/users/staff/list'
-    querystring = {"withPhone": "true"}  # костыль считаю у кого по базе  есть телефон тот  может ходить по камере
-    for id, name in pd.read_json(requests.request("get", url, headers=headers, params=querystring).text)[
-        ['id', 'name']].values:
-        db[id] = name
-    print(db)
-    return db
-
-
-def read_db():
-    db = {}
-    url = f'http://{host}/api/users/staff/list'
-    querystring = {"withPhone": "true"}  # костыль считаю у кого по базе  есть телефон тот  может ходить по камере
-    for user in json.loads(requests.request("get", url, headers=headers, params=querystring).text):
-        # print(user['id'],user['name'])
-        db[user['id']] = user['name']
-    return db
-
-
-def encode_folder(folder, save_to):
-    os.makedirs(save_to, exist_ok=True)
-    for file in glob.glob(folder + '/*'):
-        enc_file = f'{save_to}/{os.path.basename(file).split(".")[0]}'
-        if not os.path.exists(enc_file):
-            image = fr.load_image_file(file)
-            encoding = fr.face_encodings(image, num_jitters=num_jitters)[0]
-            with open(enc_file, 'wb') as f:
-                pickle.dump(encoding, f)
-
-
-def get_encodings(dict_users, encoded_path):
-    encodings = []
-    ids = []  # id записы в тавлице базы совпадает с именем файла
-    for id in dict_users:  #
-        ids.append(id)
-        path = f'{encoded_path}/{id}'
-        with open(path, 'rb') as id:
-            encoding = pickle.load(id)
-        encodings.append(encoding)
-    return encodings, ids
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Face recognition')
-    parser.add_argument('-e', '--encode', type=str)
-    parser.add_argument('--db', type=str, default='db.xlsx')
-    parser.add_argument('-p', '--path', type=str, default='encoded')
-    parser.add_argument('-c', '--camera', type=str, default=0)
-    parser.add_argument('-s', '--show', action='store_true')
-
+    parser.add_argument('-e', '--encode', action="store_true")
     args = parser.parse_args()
 
     if args.encode:
-        encode_folder(args.encode, args.path)
+        encode_folder()
         exit()
+    if not config['test_mode']:
+        set_headers()
 
-    headers["Authorization"] = getToken()
-    dict_users = read_db()
-    encodings, ids = get_encodings(dict_users, args.path)
-    # open_doors(103,2)
-    print(f' start В {datetime.now().strftime("%D:%H:%M:%S")}')
-    video_capture(encodings, ids, camera=args.camera, show=args.show)
+    if config['excel_file']:
+        dict_users = read_excel(config['excel_file'])
+    else:
+        dict_users = read_db()
+    dict_users[0] = config['no_name_user']
+    id_to_encoding = get_encodings(dict_users, config[config['mode']]['embedding_folder'])
+
+    print(f'started at {datetime.now().strftime("%D:%H:%M:%S")}')
+    video_capture(id_to_encoding, camera=config['camera'], show=config['show'], dict_users=dict_users,
+                  test=config['test_mode'])
---
+++ modes/facenet.py
@@ -0,0 +1,72 @@
+import glob
+import os
+import pickle
+
+import numpy as np
+import torch
+from PIL import Image
+from facenet_pytorch import MTCNN, InceptionResnetV1
+
+from config import config
+from utils import face_in_area
+import tqdm
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+mtcnn_scene = MTCNN(
+    image_size=160, margin=0, min_face_size=20,
+    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True, keep_all=True,
+    device=device)
+resnet = InceptionResnetV1(pretrained='vggface2').eval()
+
+
+def encode_folder():
+    save_to = config['facenet']['embedding_folder']
+    mtcnn_encode = MTCNN(
+        image_size=160, margin=0, min_face_size=20,
+        thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+        device=device)
+    os.makedirs(save_to, exist_ok=True)
+    for file in tqdm.tqdm(glob.glob(config['images_folder'] + '/*')):
+        enc_file = f'{save_to}/{os.path.basename(file).split(".")[0]}'
+        img = Image.open(file)
+        if not os.path.exists(enc_file):
+            img_cropped = mtcnn_encode(img)
+            encoding = resnet(img_cropped.unsqueeze(0).to(device)).detach().cpu()
+            with open(enc_file, 'wb') as f:
+                pickle.dump(encoding, f)
+
+
+def search_for_faces(id_to_encoding, frame, left_area, right_area):
+    face_locations, _ = mtcnn_scene.detect(frame, landmarks=False)
+    recognized_names = []
+    open_door = -1
+    open_name = None
+    if face_locations is None or len(face_locations) == 0:
+        return [], [], open_door, open_name
+    try:
+        faces = mtcnn_scene.extract(frame, face_locations, save_path='faces/temp.jpg')
+    except:
+        return [], [], open_door, open_name
+
+    face_locations = np.array(face_locations)
+    face_locations[:, [0, 1, 2, 3]] = face_locations[:, [1, 0, 3, 2]]
+    faces = faces.to(device)
+    img_embeddings = resnet(faces).detach().cpu()
+    names = list(id_to_encoding.keys())
+    encodings = list(id_to_encoding.values())
+    for i, embedding in enumerate(img_embeddings):
+        name = 0
+        distances = [(embedding - e).norm().item() for e in encodings]
+        best_match_index = np.argmin(distances)
+        if distances[best_match_index] < config['facenet']['threshold']:
+            name = names[best_match_index]
+            if face_in_area(face_locations[i], left_area):
+                open_name = name
+                open_door = 2
+            elif face_in_area(face_locations[i], right_area):
+                open_name = name
+                open_door = 1
+        recognized_names.append(name)
+
+    return recognized_names, face_locations, open_door, open_name
