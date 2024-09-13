244744d500ea4b555b68bad1dba1e9ef454486ca
Maria Piliugina <mary905el@gmail.com>
2024-09-13 18:28:02 +0500

    Added recognition models testing

--- modes/face_recognition.py
+++ modes/face_recognition.py
@@ -5,9 +5,16 @@ import pickle
 import face_recognition as fr
 import numpy as np
 import tqdm
+from PIL import Image
 
 from config import config
 from utils import face_in_area
+
+
+def encode_image(image, known_location=None):
+    if type(image) != np.array:
+        image = np.array(image)
+    return fr.face_encodings(image, known_location, num_jitters=config['face-recognition']['num_jitters'])
 
 
 def encode_folder():
@@ -18,9 +25,18 @@ def encode_folder():
         enc_file = f'{save_to}/{os.path.basename(file).split(".")[0]}'
         if not os.path.exists(enc_file):
             image = fr.load_image_file(file)
-            encoding = fr.face_encodings(image, num_jitters=config['face-recognition']['num_jitters'])[0]
+            encoding = encode_image(image)[0]
             with open(enc_file, 'wb') as f:
                 pickle.dump(encoding, f)
+
+
+def get_best_match_idx(encodings: list, face_encoding: np.array):
+    matches = fr.compare_faces(encodings, face_encoding, tolerance=config['face-recognition']['tolerance'])
+    face_distances = fr.face_distance(encodings, face_encoding)
+    best_match_index = np.argmin(face_distances)
+    if not matches[best_match_index]:
+        return None
+    return best_match_index
 
 
 def search_for_faces(id_to_encoding, small_frame, left_area, right_area):
@@ -28,7 +44,7 @@ def search_for_faces(id_to_encoding, small_frame, left_area, right_area):
     if len(face_locations) == 0:
         return [], [], -1, None
     # face_locations
-    face_encodings = fr.face_encodings(small_frame, face_locations)
+    face_encodings = encode_image(small_frame, face_locations)
     face_locations = np.array(face_locations)
     face_locations[:, [1, 3]] = face_locations[:, [3, 1]]
     recognized_names = []
@@ -37,11 +53,9 @@ def search_for_faces(id_to_encoding, small_frame, left_area, right_area):
     names = list(id_to_encoding.keys())
     encodings = list(id_to_encoding.values())
     for i, face_encoding in enumerate(face_encodings):
-        matches = fr.compare_faces(encodings, face_encoding, tolerance=config['face-recognition']['tolerance'])
         name = 0
-        face_distances = fr.face_distance(encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
+        best_match_index = get_best_match_idx(encodings, face_encoding)
+        if best_match_index is not None:
             name = names[best_match_index]
             if face_in_area(face_locations[i], left_area):
                 open_name = name
--- modes/facenet.py
+++ modes/facenet.py
@@ -17,24 +17,37 @@ mtcnn_scene = MTCNN(
     image_size=160, margin=0, min_face_size=20,
     thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True, keep_all=True,
     device=device)
+mtcnn_encode = MTCNN(
+        image_size=160, margin=0, min_face_size=20,
+        thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+        device=device)
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
+
+
+def encode_image(image):
+    img_cropped = mtcnn_encode(image)
+    encoding = resnet(img_cropped.unsqueeze(0).to(device)).detach().cpu()
+    return [encoding]
 
 
 def encode_folder():
     save_to = config['facenet']['embedding_folder']
-    mtcnn_encode = MTCNN(
-        image_size=160, margin=0, min_face_size=20,
-        thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
-        device=device)
     os.makedirs(save_to, exist_ok=True)
     for file in tqdm.tqdm(glob.glob(config['images_folder'] + '/*')):
         enc_file = f'{save_to}/{os.path.basename(file).split(".")[0]}'
         img = Image.open(file)
         if not os.path.exists(enc_file):
-            img_cropped = mtcnn_encode(img)
-            encoding = resnet(img_cropped.unsqueeze(0).to(device)).detach().cpu()
+            encoding = encode_image(img)[0]
             with open(enc_file, 'wb') as f:
                 pickle.dump(encoding, f)
+
+
+def get_best_match_idx(encodings, embedding):
+    distances = [(embedding - e).norm().item() for e in encodings]
+    best_match_index = np.argmin(distances)
+    if distances[best_match_index] >= config['facenet']['threshold']:
+        return None
+    return best_match_index
 
 
 def search_for_faces(id_to_encoding, frame, left_area, right_area):
@@ -57,9 +70,8 @@ def search_for_faces(id_to_encoding, frame, left_area, right_area):
     encodings = list(id_to_encoding.values())
     for i, embedding in enumerate(img_embeddings):
         name = 0
-        distances = [(embedding - e).norm().item() for e in encodings]
-        best_match_index = np.argmin(distances)
-        if distances[best_match_index] < config['facenet']['threshold']:
+        best_match_index = get_best_match_idx(encodings, embedding)
+        if best_match_index is not None:
             name = names[best_match_index]
             if face_in_area(face_locations[i], left_area):
                 open_name = name
